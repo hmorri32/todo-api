fe33f88427b13c139c4296f12df551da0138c5aa
Hugh Morrison <hmorri32@gmail.com>
2017-11-25 21:01:38 -0500

    user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+  end
+
+  context "relationships" do
+    it { should have_many(:todos) }
+  end
 end
